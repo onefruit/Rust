ebb7029c8ee952c40fb5ea0dbe6c3d07a60b4e15
Prabin-sc1 <prabin.sc1@gmail.com>
2025-09-01 09:49:45 +0545

    feat: ownership concept

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+
+    // let s1:String 
+
+    let s1:String = get_string();
+    println!("This is s1: {}",s1);
+
+    let s2:String = String::from("world");
+    let s3:String = send_get_string(s2);
+
+    println!("THis is s3: {}",s3);
+
+
+}
+
+fn get_string()->String{
+    let new_string = String::from("hello");
+    return new_string;
+}
+
+fn send_get_string(received_string:String)->String{
+    return received_string;
+}
