99bbddb7e4161b5bdcad125026c48c7ffa5a7b56
Prabin-sc1 <prabin.sc1@gmail.com>
2025-09-19 17:44:12 +0545

    feat: random number guessing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
- println!("prabin");   
+    let mut rng = rand::thread_rng();
+
+    let num1: i32 = rng.gen_range(1..=10);
+
+    let mut input = String::new();
+    
+    println!("Guess a number between 1 and 10:");
+    io::stdin().read_line(&mut input)
+    .expect("failed to read input");
+
+
+    let input_num: i32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a valid number!");
+            return;
+        }
+    };
+   
+
+    if num1 == input_num {
+        println!("You win!");
+    } else {
+        println!("Try again! The number was {}", num1);
+    }
 }
